ab027e9fce40991200371746d4ce6888fb747938
Skyline793 <ivan.ru614@gmail.com>
2022-10-01 18:02:28 +0700

    Добавил и описал класс треугольник

---
+++ Triangle.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Triangle
+{
+	private Point A, B, C;
+	
+	Triangle()
+	{
+		A = new Point();
+		B = new Point();
+		C = new Point();
+	}
+	
+	Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
+	{
+		A = new Point(x1, y1);
+		B = new Point(x2, y2);
+		C = new Point(x3, y3);
+	}
+	
+	public void Init(int x1, int y1, int x2, int y2, int x3, int y3)
+	{
+		A.Init(x1, y1);
+		B.Init(x2, y2);
+		C.Init(x3, y3);
+	}
+	
+	public void Read()
+	{
+		int x1, y1, x2, y2, x3, y3;
+		Scanner inp = new Scanner(System.in);
+		System.out.print("Введите координаты вершины A треугольника: ");
+		x1 =inp.nextInt();
+		y1 =inp.nextInt();
+		System.out.print("Введите координаты вершины B треугольника: ");
+		x2 =inp.nextInt();
+		y2 =inp.nextInt();
+		System.out.print("Введите координаты вершины C треугольника: ");
+		x3 =inp.nextInt();
+		y3 =inp.nextInt();
+		inp.close();
+		A.Init(x1, y1);
+		B.Init(x2, y2);
+		C.Init(x3, y3);
+	}
+	
+	public void Display()
+	{
+		System.out.print("Треугольник с вершинами A");
+		A.Display();
+		System.out.print(", B");
+		B.Display();
+		System.out.print(", C");
+		C.Display();
+		System.out.println();
+	}
+	
+	public double CalcSquare()
+	{
+		int x1 = A.GetX(), y1 = A.GetY();
+		int x2 = B.GetX(), y2 = B.GetY();
+		int x3 = C.GetX(), y3 = C.GetY();
+		double S;
+		S = 0.5 * abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
+		return S;
+	}
+	
+	public double CalcPerimeter()
+	{
+		int x1 = A.GetX(), y1 = A.GetY();
+		int x2 = B.GetX(), y2 = B.GetY();
+		int x3 = C.GetX(), y3 = C.GetY();
+		double AB = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		double AC = sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+		double BC = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+		double P = AB + AC + BC;
+		return P;
+	}
+}
--- lab3.java
+++ lab3.java
@@ -2,14 +2,14 @@ public class lab3
 {
 	public static void main(String[] args)
 	{
-		Circle c = new Circle(1, 1, 1.2);
-		c.Display();
-		c.PrintEquation();
-		System.out.printf("S=%.3f", c.CalcSquare());
-
-		c.Read();
-		c.Display();
-		c.PrintEquation();
-		System.out.printf("S=%.3f", c.CalcSquare());
+		Triangle t = new Triangle(3, 1, 0, 5, -4, 3);
+		t.Display();
+		System.out.printf("S=%.3f, P=%.3f\n", t.CalcSquare(), t.CalcPerimeter());
+		t.Init(1, 2, -3, -4, 5, -6);
+		t.Display();
+		System.out.printf("S=%.3f, P=%.3f\n", t.CalcSquare(), t.CalcPerimeter());
+		t.Read();
+		t.Display();
+		System.out.printf("S=%.3f, P=%.3f\n", t.CalcSquare(), t.CalcPerimeter());
 	}
 }
